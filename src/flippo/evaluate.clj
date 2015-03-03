89427ffdc6b6fc49657b0f7bbedb43d1f05152e1
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-03 07:45:22 +0100

    Simplify docstring of evaluate

--- src/flippo/evaluate.clj
+++ src/flippo/evaluate.clj
@@ -1,8 +1,8 @@
 (ns flippo.evaluate)
 
 (defn evaluate [tree]
-  "Evaluates a tree representation like {:operator
-   #(+ %1 %2) :left {:value 1} :right {:value 2}}
+  "Evaluates a tree representation like {:operator +
+   :left {:value 1} :right {:value 2}}
    to the corresponding value, i.e. 3."
   (if (:operator tree)
     ((:operator tree) (evaluate (:left tree)) (evaluate (:right tree)))
