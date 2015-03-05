1843ab1a0f6fc4df9c487882dba32ae0f22c669c
dvberkel <daan.v.berkel.1980@gmail.com>
2015-03-05 13:43:59 +0100

    Clean up doc string

--- src/flippo/evaluate.clj
+++ src/flippo/evaluate.clj
@@ -1,17 +1,15 @@
 (ns flippo.evaluate)
 
 (defn evaluate [tree]
-  "Evaluates a tree representation like {:operator +
-   :left {:value 1} :right {:value 2}}
+  "Evaluates a tree representation like {:operator + :left {:value 1} :right {:value 2}}
    to the corresponding value, i.e. 3."
   (if (:operator tree)
     ((:operator tree) (evaluate (:left tree)) (evaluate (:right tree)))
     (:value tree)))
 
 (defn safe-evaluate [tree]
-  "Evaluates a tree representation like {:operator +
-   :left {:value 1} :right {:value 2}} to the corresponding value, i.e. 3.
-   Returns nil on ArithmiticException"
+  "Evaluates a tree representation like {:operator + :left {:value 1} :right {:value 2}}
+   to the corresponding value, i.e. 3. Returns nil on ArithmiticException"
   (try
     (evaluate tree)
     (catch ArithmeticException e nil)))
