0f7b9d0cad12a11af829fad3dfe4231ae09e0c56
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-10 10:50:11 +0200

    Provide a description and an url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject flippo "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Solve 24 number problems"
+  :url "https://github.com/dvberkel/flippo"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
