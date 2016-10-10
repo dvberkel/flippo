d08b29326b579bda62d6d81858d8bd7cb9c39776
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-10 11:02:42 +0200

    Depend on clojurescript

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/math.combinatorics "0.1.3"]]
+                 [org.clojure/math.combinatorics "0.1.3"]
+                 [org.clojure/clojurescript "1.9.229"]]
   :main ^:skip-aot flippo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
