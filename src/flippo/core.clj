b0eb4e7bc614338de420b9e779f77b92a8534b1b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-12-10 21:04:36 +0100

    transform core into a true clj file

--- src/flippo/core.clj
+++ src/flippo/core.clj
@@ -1,9 +1,9 @@
 (ns flippo.core
-  #?(:clj (:gen-class))
+  (:gen-class)
   (:use [flippo.solution :only [solve]]
         [flippo.representation :only [infix]]))
 
 (defn -main
   "Print out a solution to the 24 problem given the arguments"
   [& args]
-  (println (infix(first (take 1 (solve (vec (map #(Integer/valueOf %) args))))))))
+  (println (infix (first (take 1 (solve (vec (map #(Integer/valueOf %) args))))))))
